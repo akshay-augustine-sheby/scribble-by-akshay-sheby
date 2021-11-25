e8b2ea43e5ee3b102168b035ba6b7a9fe02bfa65
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-25 23:55:53 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import { initializeLogger } from "common/logger";
 
 const App = () => {
-    useEffect(() => {
-        initializeLogger();
-    }, [])
-    
+  useEffect(() => {
+    initializeLogger();
+  }, []);
+
   return (
     <Router>
       <Switch>
